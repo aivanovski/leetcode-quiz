7bee26d1739e722196d86cc27214b2c9377ff412
Aliaksei Ivanouski <alexei.ivanovski@gmail.com>
2026-02-05 23:54:48 +0100

    [BACKEND] Add questions to /api/problmen/id response (#14)

--- backend/app/src/main/scala/com/github/ai/leetcodequiz/data/db/repository/QuestionRepository.scala
+++ backend/app/src/main/scala/com/github/ai/leetcodequiz/data/db/repository/QuestionRepository.scala
@@ -1,14 +1,18 @@
 package com.github.ai.leetcodequiz.data.db.repository
 
 import com.github.ai.leetcodequiz.data.db.dao.QuestionEntityDao
-import com.github.ai.leetcodequiz.data.db.model.{QuestionEntity, QuestionUid}
+import com.github.ai.leetcodequiz.data.db.model.{ProblemId, QuestionEntity, QuestionUid}
+import com.github.ai.leetcodequiz.entity.exception.DatabaseError
+import slick.jdbc.SQLiteProfile.api.*
 
 class QuestionRepository(
   private val dao: QuestionEntityDao
 ) {
 
   def getAll() = dao.getAll()
+  def getByUid(uid: QuestionUid) = dao.getByUid(uid)
   def add(question: QuestionEntity) = dao.add(question)
   def update(question: QuestionEntity) = dao.update(question)
-  def delete(uid: QuestionUid) = dao.deleteQuestion(uid)
+  def delete(uid: QuestionUid) = dao.deleteByUid(uid)
+  def findByProblemId(problemId: ProblemId) = dao.findByProblemId(problemId)
 }
