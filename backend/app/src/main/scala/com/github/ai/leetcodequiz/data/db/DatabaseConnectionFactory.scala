d94e58943df15c8079cd8f64adc86e3b3f032386
Aliaksei Ivanouski <alexei.ivanovski@gmail.com>
2026-02-05 22:15:24 +0100

    [BACKEND] Enable Hikari connection pool

--- backend/app/src/main/scala/com/github/ai/leetcodequiz/data/db/DatabaseConnectionFactory.scala
+++ backend/app/src/main/scala/com/github/ai/leetcodequiz/data/db/DatabaseConnectionFactory.scala
@@ -1,8 +1,8 @@
 package com.github.ai.leetcodequiz.data.db
 
 import com.github.ai.leetcodequiz.entity.exception.DatabaseError
-import com.github.ai.leetcodequiz.utils.toProperties
 import com.typesafe.config.ConfigFactory
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import zio.{IO, ZIO}
 import zio.direct.*
 import slick.jdbc.SQLiteProfile.api.*
@@ -14,38 +14,19 @@ class DatabaseConnectionFactory {
 
     val db = ZIO
       .attempt {
-        Database.forURL(
-          url = config.url,
-          user = config.user,
-          password = config.password,
-          driver = config.driverClassName,
-          keepAliveConnection = true,
-          prop = Map(
-            "connectionPool" -> "HikariCP"
-          ).toProperties()
-        )
+        val hikariConfig = new HikariConfig()
+        hikariConfig.setJdbcUrl(config.url)
+        hikariConfig.setUsername(config.user)
+        hikariConfig.setPassword(config.password)
+        hikariConfig.setDriverClassName(config.driverClassName)
+        hikariConfig.setMaximumPoolSize(config.maximumPoolSize)
+        hikariConfig.setMinimumIdle(config.minimumIdle)
+
+        val dataSource = new HikariDataSource(hikariConfig)
+        Database.forDataSource(dataSource, Some(config.maximumPoolSize))
       }
       .mapError(DatabaseError(_))
       .run
-
-    // TODO: maybe it would be better to use acquireRelease
-//    val db = ZIO
-//      .acquireRelease(
-//        ZIO.attempt(
-//          Database.forURL(
-//            url = config.url,
-//            user = config.user,
-//            password = config.password,
-//            driver = config.driverClassName,
-//            keepAliveConnection = true,
-//            prop = Map(
-//              "connectionPool" -> "HikariCP"
-//            ).toProperties()
-//          )
-//        )
-//      )(db => ZIO.attempt(db.close()).orDie)
-//      .mapError(DatabaseError(_))
-//      .run
 
     db
   }
